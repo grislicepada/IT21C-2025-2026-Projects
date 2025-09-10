188f8acd473b16de28ec1b96eb5849e5282f7afc
Ellaiza Jean Curay Balatero <biancaligmon93@gmail.com>
2025-09-10 13:52:26 +0800

    grislicepada

---
+++ cepadaLife.js
@@ -0,0 +1,8 @@
+let name = "Gracelie Mae P. Cepada";
+let course = "Bachelor of Science of Information Technology";
+let year = "2nd Year";
+let tlsleep = "8";
+let tlstudy = "2";
+
+console.log ("\nName: "+name+"\nCourse: "+course+"\nYear Level: "+year);
+console.log("\nTotal hours of sleep: "+tlsleep+"\nTotal hours of study: "+tlstudy);
