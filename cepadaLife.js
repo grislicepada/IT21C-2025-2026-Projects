7007718475400a257c4053d375f992432914ab09
Ellaiza Jean Curay Balatero <biancaligmon93@gmail.com>
2025-09-10 14:01:50 +0800

    Added Operators for Daily Routine

---
+++ cepadaLife.js
@@ -0,0 +1,8 @@
+let name = "Gracelie Mae P. Cepada";
+let course = "Bachelor of Science of Information Technology";
+let year = "2nd Year";
+let tlsleep = "8";
+let tlstudy = "2";
+
+console.log("\nName: "+name+"\nCourse: "+course+"\nYear Level: "+year);
+console.log("\nTotal Hours of Sleep: "+tlsleep+"\nTotal Hours of Sleep: "+tlstudy);
